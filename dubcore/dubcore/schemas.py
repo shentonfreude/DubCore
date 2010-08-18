69308f1969b1b2d3e4c673b3ce5bf8e3d29f0593
Chris Shenton <chris@shenton.org>
2010-08-18 18:27:31 -0400

    Make Sequence schemas render labels; add descrs.

---
+++ dubcore/dubcore/schemas.py
@@ -0,0 +1,64 @@
+from colander import Schema, SchemaNode, SequenceSchema, MappingSchema
+from colander import String, Date, null
+from colander import OneOf
+import datetime
+
+LANGUAGE_LABELS = ['English', 'Spanish', 'Esperanto']
+LANGUAGES = zip(LANGUAGE_LABELS, LANGUAGE_LABELS)
+
+FORMAT_CHOICES = ['text/html', 'text/rst', 'text/plain']
+FORMAT_LABELS = ['html', 'rst', 'text']
+FORMATS = zip(FORMAT_CHOICES, FORMAT_LABELS)
+
+class Creators(SequenceSchema):
+    creator = SchemaNode(String(), missing='', title="Creator Name")
+
+class Subjects(SequenceSchema):
+    subject = SchemaNode(String(),
+                         title="Subject Keyword",
+                         description="Keyword or Tag",
+                         missing='')
+Subjects.category = None
+
+class Contributors(SequenceSchema):
+    contributor_name = SchemaNode(String(),
+                                  title="Contributor Name",
+                                  missing='')
+Contributors.category = None
+
+class DublinCoreSchema(MappingSchema):
+    title = SchemaNode(String())
+    description = SchemaNode(
+        String(), missing='',
+        description="Short summary returned in search results")
+    date = SchemaNode(Date(),  default=datetime.date.today(),
+                      description="Created, Modified, ...")
+    subject = Subjects(missing=null)
+    creator = Creators(missing=null)
+    contributor = Contributors(missing=null)
+    publisher = SchemaNode(String(),  missing='')
+    format = SchemaNode(String(),  missing='',
+                        validator=OneOf(FORMAT_CHOICES))
+    language = SchemaNode(String(),  missing='', # should use AutoComplete
+                          validator=OneOf(LANGUAGE_LABELS))
+    type_ = SchemaNode(String(),  name="type", missing='',
+                       description="DCMI Type Vocabulary")
+    identifier = SchemaNode(String(),  missing='',
+                            description="URI")
+    source = SchemaNode(String(),  missing='',
+                        description="URI")
+    relation = SchemaNode(String(),  missing='',
+                          description="Is version of, Is part of, ...")
+    coverage = SchemaNode(String(),  missing='',
+                          description="Spatial and temporal characteristics",)
+    rights = SchemaNode(String(),  missing='')
+
+class PageSchema(Schema):
+    """Get Page fields and Plone-style uber-common DublinCore fields.
+    __name__ like Plone 'id' generated from Title.
+    """
+    title = DublinCoreSchema.title
+    description = DublinCoreSchema.description
+    data = SchemaNode(String(), description="Data for the page")
+
+
--- dubcore/dubcore/views.py
+++ dubcore/dubcore/views.py
@@ -1,70 +1,23 @@
+# TODO: change __name__ if Title changes in Page or DublinCore (or not?)
+
 import datetime
 from util import canonize
 from webob.exc import HTTPFound
 from models import Page
 from repoze.bfg.url import model_url
-from colander import Schema, SchemaNode, String, Date
-from colander import MappingSchema, SequenceSchema
-from colander import OneOf
+from repoze.bfg.settings import get_settings
 from deform import Form
 from deform import widget
 from deform import ValidationFailure
-from repoze.bfg.settings import get_settings
+from schemas import DublinCoreSchema, PageSchema
+from schemas import LANGUAGES, FORMATS
 
 PROJECT = get_settings()['project']
-LANGUAGE_LABELS = ['English', 'Spanish', 'Esperanto']
-LANGUAGES = zip(LANGUAGE_LABELS, LANGUAGE_LABELS)
-FORMAT_LABELS = ['html', 'rst', 'text']
-FORMAT_CHOICES = ['text/html', 'text/rst', 'text/plain']
-FORMATS = zip(FORMAT_CHOICES, FORMAT_LABELS)
-
-class Creators(SequenceSchema):
-    creator = SchemaNode(String(), missing='', title="Creator Name")
-
-class Subjects(SequenceSchema):
-    subject = SchemaNode(String(),
-                         title="Subject Keyword",
-                         description="Keyword or Tag",
-                         missing='')
-
-class Contributors(SequenceSchema):
-    contributor_name = SchemaNode(String(),
-                                  title="Contributor Name",
-                                  missing='')
-
-class DublinCoreSchema(MappingSchema):
-    title = SchemaNode(String())
-    creator = Creators()
-    subject = Subjects()
-    description = SchemaNode(
-        String(), missing='',
-        description="Short summary returned in search results")
-    publisher = SchemaNode(String(),  missing='')
-    contributor = Contributors()
-    date = SchemaNode(Date(),  default=datetime.date.today())
-    type_ = SchemaNode(String(),  name="type", missing='') # WTF is this?
-    format = SchemaNode(String(),  missing='',
-                        validator=OneOf(FORMAT_CHOICES))
-    identifier = SchemaNode(String(),  missing='')
-    source = SchemaNode(String(),  missing='')
-    language = SchemaNode(String(),  missing='', # should use AutoComplete
-                          validator=OneOf(LANGUAGE_LABELS))
-    relation = SchemaNode(String(),  missing='')
-    coverage = SchemaNode(String(),  missing='')
-    rights = SchemaNode(String(),  missing='')
-
-class PageSchema(Schema):
-    """Get Page fields and Plone-style uber-common DublinCore fields.
-    __name__ like Plone 'id' generated from Title.
-    """
-    title = DublinCoreSchema.title
-    description = DublinCoreSchema.description
-    data = SchemaNode(String(), description="Data for the page")
-
 
 def pages_view(context, request):
     page_urls = [(context[p].dublincore['title'],
-                  model_url(context, request, p)) for p in request.context.keys()]
+                  model_url(context, request, p))
+                 for p in request.context.keys()]
     return {'project': PROJECT,
             'page_urls': page_urls,
             'page_add_url': model_url(context, request, "@@page_add")
@@ -73,6 +26,7 @@ def pages_view(context, request):
 def page_view(context, request):
     dc_schema = DublinCoreSchema()
     dc_form = Form(dc_schema)
+    dc_form['subject'].widget.category = None # HACK: display sequence label
     return {'project': PROJECT,
             'page': context,
             'dc_form': dc_form.render(context.dublincore, readonly=True),
@@ -85,7 +39,7 @@ def page_add(context, request):
     schema = PageSchema()
     form = Form(schema, buttons=('submit',))
     form['data'].widget = widget.RichTextWidget(width=390, theme="advanced")
-    if 'submit' in request.params: # or method==POST
+    if 'submit' in request.params:
         controls = request.POST.items()
         try:
             appstruct = form.validate(controls)
@@ -100,7 +54,6 @@ def page_add(context, request):
                       }
         page = Page(appstruct['data'], dublincore=dublincore)
         __name__ = canonize(appstruct['title'])
-        # TODO: if title changed remove old __name__
         context[__name__] = page
         return HTTPFound(location=model_url(page, request))
     return {'project': PROJECT,
@@ -108,13 +61,12 @@ def page_add(context, request):
             'add_or_edit': add_or_edit,
             }
 
-# TODO: change __name__ if Title changed
 def page_edit(context, request):
     add_or_edit = 'Edit'
     schema = PageSchema()
     form = Form(schema, buttons=('submit',))
     form['data'].widget = widget.RichTextWidget(theme="advanced")
-    if 'submit' in request.params: # or method==POST
+    if 'submit' in request.params:
         controls = request.POST.items()
         try:
             appstruct = form.validate(controls)
@@ -122,7 +74,6 @@ def page_edit(context, request):
             return {'project': PROJECT,
                     'form': e.render(),
                     'add_or_edit': add_or_edit}
-        # TODO: if title changed remove old __name__
         context.data = appstruct['data']
         context.dublincore['title'] = appstruct['title']
         context.dublincore['description'] = appstruct['description']
@@ -138,15 +89,18 @@ def page_edit(context, request):
             'add_or_edit': add_or_edit,
             }
 
-# TODO: change __name__ if Title changed
 def page_edit_dc(context, request):
     """Edit the DublinCore attributes of a Page."""
     schema = DublinCoreSchema()
     form = Form(schema, buttons=('submit',))
-    form['rights'].widget = widget.RichTextWidget()
+    form['description'].widget = widget.TextAreaWidget(rows=10, cols=40)
+    form['subject'].widget.category = None     # HACK: display sequence label
+    form['creator'].widget.category = None     # HACK: display sequence label
+    form['contributor'].widget.category = None # HACK: display sequence label
+    form['rights'].widget = widget.TextAreaWidget(rows=10, cols=40)
     form['language'].widget = widget.SelectWidget(values=LANGUAGES)
     form['format'].widget = widget.SelectWidget(values=FORMATS)
-    if 'submit' in request.params: # or method==POST
+    if 'submit' in request.params:
         controls = request.POST.items()
         try:
             appstruct = form.validate(controls)
